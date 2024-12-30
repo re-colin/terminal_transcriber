95a02657c53fc71afcbd7d67894a23b834442ca2
re-colin <stinkbugget@gmail.com>
2024-12-30 13:08:55 +0000

    Basic Whisper template in main.py / pip freeze requirements

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import whisper
+import time
+import os
 
+default_directory = ""
+transcripton_queue = []
+
+models = [
+    "small",
+    "medium",
+    "large"
+]
+
+def transcribe_start(file, default_directory):
+    start = time.time()
+    model = whisper.load_model(name = models[0])
+
+    result = model.transcribe(file)
+    
+    print("\nTRANSCRIPTION COMPLETE: \n")
+    print(result['text'])
+
+    with open(default_directory, 'w') as f:
+        f.write(result['text'])
+
+    end = time.time()
+    total_time = end - start
+    print(f'TRANSCRIPTION RUNTIME: {total_time} secs. ')
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
