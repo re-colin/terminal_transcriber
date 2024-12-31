1b8ff51a56564a6c71368d26bd450effd55323b4
re-colin <stinkbugget@gmail.com>
2024-12-31 16:56:10 +0000

    Added check_queue template / go techeck how to use json later

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import whisper
 import time
 import os
+import json
 
-print("colin had a syncing conflict this is a test")
-
-default_directory = ""
+input_directory = ""
+output_directory = ""
 transcripton_queue = []
 
 models = [
@@ -13,20 +13,45 @@ models = [
     "large"
 ]
 
-def transcribe_start(file, model_select):
-    start = time.time()
-    model = whisper.load_model(name = models[model_select])
+def write_to_output(result, output):
+    with open(f"{output}/{result}.md") as output_file:
+        output_file.write(result)
 
-    print(f"Model selected: {models[model_select]}")
+
+def create_queue():
+    pass
+
+
+def check_queue():
+    exists = False
+
+    for file in (os.listdir(input_directory)):
+        if file in os.listdir(output_directory):
+            exists = True
+            transcripton_queue.remove(file)
+            return 1
+        
+    return -1 
+
+
+
+def transcribe_start(file, model_sel):
+    start = time.time()
+    model = whisper.load_model(name = models[model_sel])
+
+    print(f"Model selected: {models[model_sel]}")
     print(f"TRANSCRIBING...")
 
-    result = model.transcribe(file)
+    audio = whisper.load_audio(file)
+    # Below line throws a FileNotFoundError
+    # assuming its related to ffmpeg-python not being installed
+    # there should be no problem with how the audio sample is being referenced
+    result = model.transcribe(audio)
     
     print("\nTRANSCRIPTION COMPLETE: \n")
     print(result['text'])
 
-    with open(default_directory, 'w') as f:
-        f.write(result['text'])
+    write_to_output(result, output_directory)
 
     end = time.time()
     total_time = end - start
@@ -35,13 +60,16 @@ def transcribe_start(file, model_select):
 
 def main():
     cwd = os.getcwd()
-    cwd = "C:/Users/Mark Boda/whisper-queuer"
-    file = f"/audio_samples/Bots Bridges and Euler Circuits.mp4"
+    file = f"/Bots Bridges and Euler Circuits.mp4"
+    
     input = f"{cwd}{file}"
 
     print(input)
 
-    transcribe_start(input, 0)
+    if check_queue() == -1:
+        pass
+
+    transcribe_start(file, 0)
 
 if __name__ == "__main__":
     main()
