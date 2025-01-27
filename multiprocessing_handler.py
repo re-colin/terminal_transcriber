fb3e2019bebc204272e1e230eebfa86c92dde682
re-colin <stinkbugget@gmail.com>
2025-01-27 16:37:10 +0000

    added template to multiprocessing_handler.py

--- multiprocessing_handler.py
+++ multiprocessing_handler.py
@@ -1,7 +1,21 @@
 import multiprocessing
+import threading
 
-def handle_multi_process(func, args, maximum_processes_allowed):
-    pass
+# Manage max thread/process count
+# Add setting to settings.json
 
-def handle_multi_thread(func, args, maximum_threads_allowed):
-    pass
+def handle_multi_process(func, args):
+    new_process = multiprocessing.Process(target=func, args=(args,))
+
+    new_process.start()
+
+    new_process.join()
+
+
+def handle_multi_thread(func, args):
+    new_thread = threading.Thread(target=func, args=(args,))
+
+    new_thread.start()
+
+    new_thread.join()
+
